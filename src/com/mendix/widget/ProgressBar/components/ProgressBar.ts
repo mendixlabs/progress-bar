bc2c35d973e44a94cd6f06c255ed12956d035a4b
Edwin P. Magezi <emagezi@gmail.com>
2017-01-24 16:31:13 +0300

    Add Alert component

--- src/com/mendix/widget/ProgressBar/components/ProgressBar.ts
+++ src/com/mendix/widget/ProgressBar/components/ProgressBar.ts
@@ -1,21 +1,22 @@
-import { Component, DOM } from "react";
-
+import { Component, DOM, createElement } from "react";
 import * as classNames from "classnames";
+
+import { Alert } from "./Alert";
 
 interface ProgressBarProps {
     barType?: BarType;
     bootstrapStyle?: BootstrapStyle;
-    colorSwitch: number;
+    colorSwitch?: number;
     contextObjectGuid?: string;
-    maximumValue?: number;
+    maximumValue: number;
     onClickMicroflow?: string;
-    progress: number;
+    progress: number | null;
 }
 
 type BootstrapStyle = "default" | "info" | "primary" | "success" | "warning" | "danger";
 type BarType = "default" | "striped" | "animated";
 
-class ProgressBar extends Component<ProgressBarProps, {}> {
+class ProgressBar extends Component<ProgressBarProps, { alertMessage: string }> {
     static defaultProps: ProgressBarProps = {
         barType: "default",
         bootstrapStyle: "default",
@@ -24,50 +25,57 @@ class ProgressBar extends Component<ProgressBarProps, {}> {
         progress: 0
     };
 
+    constructor(props: ProgressBarProps) {
+        super(props);
+
+        this.state = { alertMessage: "" };
+    }
+
     render() {
         const { barType, bootstrapStyle, colorSwitch, maximumValue, onClickMicroflow, progress } = this.props;
         const percentage = this.progressValue(progress, maximumValue);
-        return DOM.div(
-            {
-                className: classNames("progress", "widget-progressbar", {
-                    "red-progressbar-text": maximumValue < 1,
-                    "widget-progressbar-clickable": !!onClickMicroflow,
-                    "widget-progressbar-text-contrast": percentage < colorSwitch
-                }),
-                onClick: () => this.executeMicroflow(onClickMicroflow, this.props.contextObjectGuid)
-            },
+        return DOM.div({ className: "widget-progressbar" },
             DOM.div(
                 {
-                    className: classNames("progress-bar", `progress-bar-${bootstrapStyle}`, {
-                        "active": barType === "animated",
-                        "progress-bar-striped": barType === "striped" || barType === "animated",
-                        "widget-progressbar-negative": percentage < 0
+                    className: classNames("progress", {
+                        "red-progressbar-text": maximumValue < 1,
+                        "widget-progressbar-clickable": !!onClickMicroflow,
+                        "widget-progressbar-text-contrast": percentage < colorSwitch
                     }),
-                    style: { width: `${Math.abs(percentage)}%` }
+                    onClick: () => this.executeMicroflow(onClickMicroflow, this.props.contextObjectGuid)
                 },
-                progress
-                    ? maximumValue < 1 ? "Invalid" : `${this.calculatePercentage(progress, maximumValue)}%`
-                    : ""
-            )
+                DOM.div(
+                    {
+                        className: classNames("progress-bar", `progress-bar-${bootstrapStyle}`, {
+                            "active": barType === "animated",
+                            "progress-bar-striped": barType === "striped" || barType === "animated",
+                            "widget-progressbar-negative": percentage < 0
+                        }),
+                        style: { width: `${Math.abs(percentage)}%` }
+                    },
+                    progress
+                        ? maximumValue < 1 ? "Invalid" : `${this.calculatePercentage(progress, maximumValue)}%`
+                        : ""
+                )
+            ),
+            this.state.alertMessage ? createElement(Alert, { message: this.state.alertMessage }) : null
         );
     }
 
-    private progressValue(progress: number, maximumValue: number | undefined) {
-        const maxValue = typeof maximumValue === "undefined" ? 100 : maximumValue;
-
+    private progressValue(progress: number | null, maximumValue: number) {
         if (typeof progress !== "number") {
             return 0;
-        } else if (maxValue < 1) {
-            window.logger.warn("The maximum value is less than one. Progress is set to Invalid");
+        } else if (maximumValue < 1) {
+            window.console.warn("The maximum value is less than one. Progress is set to Invalid");
             return 0;
-        } else if (progress > maxValue || Math.abs(this.calculatePercentage(progress, maxValue)) > 100) {
+        } else if (progress > maximumValue || Math.abs(this.calculatePercentage(progress, maximumValue)) > 100) {
             return 100;
         }
 
-        return this.calculatePercentage(progress, maxValue);
+        return this.calculatePercentage(progress, maximumValue);
     }
 
-    private calculatePercentage(progress: number, maxValue: number | undefined = 100) {
+    private calculatePercentage(progress: number, maxValue: number) {
         return Math.round((progress / maxValue) * 100);
     }
 
@@ -75,7 +83,7 @@ class ProgressBar extends Component<ProgressBarProps, {}> {
         if (action && guid) {
             window.mx.ui.action(action, {
                 error: (error: Error) => {
-                    window.mx.ui.error(`Error while executing microflow: ${action}: ${error.message}`);
+                    this.setState({ alertMessage: `Error while executing microflow: ${action}: ${error.message}` });
                 },
                 params: {
                     applyto: "selection",
