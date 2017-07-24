495cda8141f8961eb40b0acc7c68dc67d9e990c4
McDoyen <mrpoloh@yahoo.co.uk>
2017-07-24 16:27:58 +0300

    Add react 16 support

--- src/components/Alert.ts
+++ src/components/Alert.ts
@@ -1,8 +1,8 @@
-import { DOM, StatelessComponent } from "react";
+import { StatelessComponent, createElement } from "react";
 
 export const Alert: StatelessComponent<{ message?: string }> = (props) =>
     props.message
-        ? DOM.div({ className: "alert alert-danger widget-progressbar-alert" }, props.message)
+        ? createElement("div", { className: "alert alert-danger widget-progressbar-alert" }, props.message)
         : null as any;
 
 Alert.displayName = "Alert";
